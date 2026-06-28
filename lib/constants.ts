b3a7bc04cb2d6ba20748595dba0f4cf246c48531
blackandb <xeurxeur1@gmail.com>
2026-06-28 21:56:12 +0300

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -13,8 +13,8 @@ export const guestRegex = /^guest-\d+$/;
 export const DUMMY_PASSWORD = generateDummyPassword();
 
 export const suggestions = [
-  "What are the advantages of using Next.js?",
-  "Write code to demonstrate Dijkstra's algorithm",
-  "Help me write an essay about Silicon Valley",
-  "What is the weather in San Francisco?",
+  "What is BLACK&?",
+  "I want to build an AI platform for my company.",
+  "Tell me about CAESAREA Technologies.",
+  "Show me investment and partnership opportunities.",
 ];
